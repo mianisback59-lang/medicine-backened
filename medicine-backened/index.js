bc41d9e5c4b7a4c99c9f35ffcf7065eaccb52ed3
Adnan Javed <mianisback59@gmail.com>
2026-08-24 21:12:12 +0500

    Added local port listening for server

--- medicine-backened/index.js
+++ medicine-backened/index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const dns = require('node:dns');
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
