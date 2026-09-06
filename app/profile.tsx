d5cd93d8becbc8e558aa58d81413dc168b8c7f3f
Adnan Javed <mianisback59@gmail.com>
2026-09-06 20:32:54 +0500

    Hide layout header and set dynamic auth state

--- app/profile.tsx
+++ app/profile.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import {
   Image,
   Modal,
+  SafeAreaView,
   ScrollView,
   StyleSheet,
   Switch,
@@ -13,7 +14,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const translations = {
   en: {
